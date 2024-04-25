6948002a48530db25f0fb9acd208aa4d6a932292
atif ahmed <atifahmedpak2k22@gmail.com>
2024-04-26 03:52:54 +0500

    Created a Simple Todo List

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let tasks = [];
+let condition = true;
+while (condition) {
+  let answer = await inquirer.prompt([
+    {
+      name: "addTodos",
+      type: "input",
+      message: "What you want to do add in your todos?",
+    },
+
+    {
+      name: "addMoreTodos",
+      type: "confirm",
+      message: "Do you want to add more todos?",
+      default: "false",
+    },
+  ]);
+  tasks.push(answer.addTodos);
+  condition = answer.addMoreTodos;
+  console.log(tasks);
+}
